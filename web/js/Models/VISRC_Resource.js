4314535f7cf925d7654ac962bd4ed2d1e129c98f
Ryan Bannon <ryan.bannon@gmail.com>
2015-07-03 11:40:52 -0400

    WIP: basic resource uploading
    Only one resource at a time for the moment.

--- web/js/Models/VISRC_Resource.js
+++ web/js/Models/VISRC_Resource.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import Backbone from 'backbone';
 import Marionette from 'backbone.marionette';
 import Radio from 'backbone.radio';
+import _ from 'underscore';
 
 import VISRC_BaseModel from './VISRC_BaseModel';
 
@@ -19,6 +20,28 @@ class VISRC_Resource extends VISRC_BaseModel
     initialize(aParameters)
     {
         this.routeName = "resources";
+    }
+
+    /**
+     * Override of sync. We do this to allow file uploads.
+     */
+    sync(aMethod, aModel, aOptions)
+    {
+        if (aMethod === "create")
+        {
+            var formData = new FormData();
+            formData.append("project", aModel.get("project"));
+            formData.append("files", aModel.get("file"));
+
+            // Set processData and contentType to false so data is sent as FormData
+            _.defaults(aOptions || (aOptions = {}), {
+                url: this.url(),
+                data: formData,
+                processData: false,
+                contentType: false
+            });
+        }
+        Backbone.sync.call(this, aMethod, aModel, aOptions);
     }
 
 ///////////////////////////////////////////////////////////////////////////////////////
