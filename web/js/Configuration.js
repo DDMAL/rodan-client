cbff95a456d5c1398572a05919bc184de1bfe988
Ryan Bannon <ryan.bannon@gmail.com>
2015-07-22 11:28:04 -0400

    MAINTENANCE: accidentally added web/js files
    So I removed them

--- web/js/Configuration.js
+++
@@ -1,7 +0,0 @@
-var Configuration = {
-    SERVER_URL: 'http://132.206.14.136',
-    SERVER_AUTHENTICATION_TYPE: 'session', // 'session' or 'token'
-    authenticationToken: null
-};
-
-export default Configuration;
