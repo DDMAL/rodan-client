bd4e9ce727ae4072e72052618284a658b0fd78aa
Michael Ryan Bannon <ryan.bannon@gmail.com>
2015-08-12 20:11:24 -0400

    MAINTENANCE: small clean-up of config-ish files

--- js/Configuration.js
+++
@@ -1,8 +0,0 @@
-var Configuration = {
-    SERVER_URL: 'http://132.206.14.136',
-    SERVER_AUTHENTICATION_TYPE: 'session', // 'session' or 'token'
-    DEBUG: true,
-    USER_AGENT: 'rodan-vis_standard'
-};
-
-export default Configuration;
