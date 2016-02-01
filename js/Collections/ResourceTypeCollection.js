4a7e48b16c6bd4d79a3f1cb11e742df4923784f5
Michael Ryan Bannon <ryan.bannon@gmail.com>
2016-02-01 08:18:55 -0500

    MAINTENANCE: only one instance of ResourceTypeCollection allowed

--- js/Collections/ResourceTypeCollection.js
+++ js/Collections/ResourceTypeCollection.js
@@ -2,6 +2,8 @@ import BaseCollection from './BaseCollection';
 import Events from '../Shared/Events';
 import ResourceType from '../Models/ResourceType';
 
+let _hasBeenInstantiated = false;
+
 /**
  * Collection of ResourceType models.
  */
@@ -15,6 +17,11 @@ class ResourceTypeCollection extends BaseCollection
      */
     initialize()
     {
+        if (_hasBeenInstantiated)
+        {
+            throw new Error('only one instance of this class may exist');
+        }
+        _hasBeenInstantiated = true;
         this.model = ResourceType;
         this.route = 'resourcetypes';
         this.loadCommand = Events.REQUEST__RESOURCETYPES_LOAD;
