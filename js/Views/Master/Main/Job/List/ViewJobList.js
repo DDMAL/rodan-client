0644d0ae93656f2429865ed4205f7445d62149f7
Michael Ryan Bannon <ryan.bannon@gmail.com>
2016-02-09 13:53:46 -0500

    FEATURE: sortable job list

--- js/Views/Master/Main/Job/List/ViewJobList.js
+++ js/Views/Master/Main/Job/List/ViewJobList.js
@@ -42,5 +42,6 @@ ViewJobList.prototype.modelEvents = {
 ViewJobList.prototype.childViewContainer = 'tbody';
 ViewJobList.prototype.template = '#template-main_job_list';
 ViewJobList.prototype.childView = ViewJobListItem;
+ViewJobList.prototype.behaviors = {Table: {'table': '#table-jobs'}};
 
 export default ViewJobList;
