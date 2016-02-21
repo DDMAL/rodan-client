4bcc46a9da500a6ba1f7d143c17b2b8c9ba722e3
Michael Ryan Bannon <ryan.bannon@gmail.com>
2016-02-21 15:30:56 -0500

    MAINTENANCE: moved resource click button handle to list view
    Moved it there as it's specific to resource lists

--- js/Views/Master/Main/Resource/List/ViewResourceList.js
+++ js/Views/Master/Main/Resource/List/ViewResourceList.js
@@ -1,7 +1,22 @@
 import BaseViewList from '../../BaseViewList';
 import Events from '../../../../../Shared/Events';
 
-class ViewResourceList extends BaseViewList {}
+class ViewResourceList extends BaseViewList
+{
+    _handleClickButtonFile()
+    {
+        var file = this.ui.fileInput[0].files[0];
+        this.ui.fileInput.replaceWith(this.ui.fileInput = this.ui.fileInput.clone(true));
+        this.rodanChannel.request(Events.REQUEST__RESOURCE_CREATE, {project: this.model, file: file});
+    }
+
+}
 ViewResourceList.prototype.behaviors = {Table: {'table': '#table-resources'}};
+ViewResourceList.prototype.ui = {
+    fileInput: '#file-main_resource_file'
+};
+ViewResourceList.prototype.events = {
+    'change @ui.fileInput': '_handleClickButtonFile'
+};
 
 export default ViewResourceList;
