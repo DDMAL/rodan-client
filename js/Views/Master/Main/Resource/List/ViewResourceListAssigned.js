70283c40e9598d0ecd3fb906f95cfe7cbc5b3b3b
Michael Ryan Bannon <ryan.bannon@gmail.com>
2016-05-06 13:44:42 -0400

    WORKING: #97
    Added method to get selected/active items from resource assignment collection.

--- js/Views/Master/Main/Resource/List/ViewResourceListAssigned.js
+++ js/Views/Master/Main/Resource/List/ViewResourceListAssigned.js
@@ -8,7 +8,19 @@ import ViewResourceListItemModal from './ViewResourceListItemModal';
 /**
  * View for assigned Resources when assigning to InputPort.
  */
-export default class ViewResourceListAssigned extends ViewResourceList {}
+export default class ViewResourceListAssigned extends ViewResourceList
+{
+	/**
+	 * Handle click.
+	 */
+	_handleClick()
+	{
+		var elementsSelection = $(this.el).find('tbody tr.active');
+	}
+}
 ViewResourceListAssigned.prototype.allowMultipleSelection = true;
 ViewResourceListAssigned.prototype.template = '#template-modal_resource_list';
 ViewResourceListAssigned.prototype.childView = ViewResourceListItemModal;
+ViewResourceListAssigned.prototype.events = {
+	'click': '_handleClick'
+}
--- js/Views/Master/Main/Resource/List/ViewResourceListAvailable.js
+++ js/Views/Master/Main/Resource/List/ViewResourceListAvailable.js
@@ -8,7 +8,16 @@ import ViewResourceListItemModal from './ViewResourceListItemModal';
 /**
  * View for available Resources when assigning to InputPort.
  */
-export default class ViewResourceListAvailable extends ViewResourceList {}
+export default class ViewResourceListAvailable extends ViewResourceList
+{
+    /**
+     * Handle click.
+     */
+    _handleClick()
+    {
+        var elementsSelection = $(this.el).find('tbody tr.active');
+    }
+}
 ViewResourceListAvailable.prototype.allowMultipleSelection = true;
 ViewResourceListAvailable.prototype.template = '#template-modal_resource_list';
 ViewResourceListAvailable.prototype.childView = ViewResourceListItemModal;
