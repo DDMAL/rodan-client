560d60e810586fcfd708a48232af3f844c0a14df
Michael Ryan Bannon <mrbannon@grieg.local>
2017-02-07 14:09:21 -0500

    WORKING #123
    added rest call

--- js/Controllers/ControllerProject.js
+++ js/Controllers/ControllerProject.js
@@ -304,25 +304,56 @@ export default class ControllerProject extends BaseController
     /**
      * Handle request add admin.
      */
-     _handleRequestProjectAddUserAdmin()
+     _handleRequestProjectAddUserAdmin(options)
      {
-
+        var users = options.project.get('admins');
+        users.push(options.username);
+        if (userIndex >= 0)
+        {
+            var ajaxSettings = {success: (response) => Radio.channel('rodan').trigger(RODAN_EVENTS.EVENT__PROJECT_ADDED_USER_ADMIN, {project: options.project}),
+                                error: (response) => Radio.channel('rodan').request(RODAN_EVENTS.REQUEST__SYSTEM_HANDLE_ERROR, {response: response}),
+                                type: 'PUT',
+                                dataType: 'json',
+                                data: users.join(),
+                                url: options.project.get('url') + 'admins/'};
+            Radio.channel('rodan').request(RODAN_EVENTS.REQUEST__SERVER_REQUEST_AJAX, {settings: ajaxSettings});
+        }
+        else
+        {
+            Radio.channel('rodan').request(RODAN_EVENTS.REQUEST__MODAL_ERROR, {content: 'An error occured trying to add this User.'});
+        }
      }
 
     /**
      * Handle request add worker.
      */
-     _handleRequestProjectAddUserWorker()
+     _handleRequestProjectAddUserWorker(options)
      {
-        
-     }
+        var users = options.project.get('workers');
+        users.push(options.username);
+        if (userIndex >= 0)
+        {
+            var ajaxSettings = {success: (response) => Radio.channel('rodan').trigger(RODAN_EVENTS.EVENT__PROJECT_ADDED_USER_WORKER, {project: options.project}),
+                                error: (response) => Radio.channel('rodan').request(RODAN_EVENTS.REQUEST__SYSTEM_HANDLE_ERROR, {response: response}),
+                                type: 'PUT',
+                                dataType: 'json',
+                                data: users.join(),
+                                url: options.project.get('url') + 'workers/'};
+            Radio.channel('rodan').request(RODAN_EVENTS.REQUEST__SERVER_REQUEST_AJAX, {settings: ajaxSettings});
+        }
+        else
+        {
+            Radio.channel('rodan').request(RODAN_EVENTS.REQUEST__MODAL_ERROR, {content: 'An error occured trying to add this User.'});
+        }
+    }
 
     /**
      * Handle event added admin.
      */
      _handleEventProjectAddedUserAdmin()
      {
-
+        this._activeProject.fetch();
+        Radio.channel('rodan').trigger(RODAN_EVENTS.EVENT__PROJECT_USERS_SELECTED, {project: this._activeProject});
      }
 
     /**
@@ -330,6 +361,7 @@ export default class ControllerProject extends BaseController
      */
      _handleEventProjectAddedUserWorker()
      {
-        
+        this._activeProject.fetch();
+        Radio.channel('rodan').trigger(RODAN_EVENTS.EVENT__PROJECT_USERS_SELECTED, {project: this._activeProject});
      }
 }
