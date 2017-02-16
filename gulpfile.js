04095726098ea56fcfdb840026d2460afbc323ff
Michael Ryan Bannon <mrbannon@grieg.local>
2017-02-16 16:43:03 -0500

    MAINTENANCE: simplify install and dev
    - npm install
    - npm start
    
    you're now running dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,13 @@
 'use strict';
 
 ////////////////////////////////////////////////////////////////////////////////
-// CONFIGURATION - Develop
-////////////////////////////////////////////////////////////////////////////////
-const DEVELOP_HOST = '132.206.14.112';
-const DEVELOP_PORT = 9002;
-const DEVELOP_SOURCEMAP = 'eval-source-map';
-
-////////////////////////////////////////////////////////////////////////////////
-// NOTE: don't edit this unless you know what you're doing.
-////////////////////////////////////////////////////////////////////////////////
-const CONFIGURATION_FILE = 'configuration.json';
-const DEVELOP_WEBROOT = '__develop__';
-const ENTRY_FILE = './js/main.js';
-const INFO_FILE = 'info.json';
-const OUTPUT_FILE = 'rodan_client.min.js';
-const PACKAGE_FILE = 'package.json';
-const PLUGINS_DIRECTORY = 'plugins';
-const RESOURCES_DIRECTORY = 'resources';
-const SOURCE_DIRECTORY = 'js';
-const TEMPLATE_DIRECTORY = 'templates';
-
-////////////////////////////////////////////////////////////////////////////////
 // REQUIRE
 ////////////////////////////////////////////////////////////////////////////////
 const del = require('del');
 const execSync = require('child_process').execSync;
 const fs = require('fs');
 const gulp = require('gulp');
+const ip = require('ip');
 const path = require('path');
 const recread = require('recursive-readdir');
 const sass = require('gulp-sass');
@@ -35,6 +15,29 @@ const shell = require('gulp-shell');
 const symlink = require('gulp-sym');
 const webpack = require('webpack');
 const WebpackDevServer = require("webpack-dev-server");
+
+////////////////////////////////////////////////////////////////////////////////
+// CONFIGURATION - Develop
+////////////////////////////////////////////////////////////////////////////////
+const DEVELOP_HOST = ip.address();
+const DEVELOP_PORT = 9002;
+const DEVELOP_SOURCEMAP = 'eval-source-map';
+const DEVELOP_WEBROOT = '__develop__';
+
+////////////////////////////////////////////////////////////////////////////////
+// NOTE: don't edit this unless you know what you're doing.
+////////////////////////////////////////////////////////////////////////////////
+const CONFIGURATION_FILE = 'configuration.json';
+const ENTRY_FILE = './js/main.js';
+const INFO_FILE = 'info.json';
+const NODE_MODULES_DIRECTORY = 'node_modules';
+const OUTPUT_FILE = 'rodan_client.min.js';
+const PACKAGE_FILE = 'package.json';
+const PLUGINS_DIRECTORY = 'plugins';
+const RESOURCES_DIRECTORY = 'resources';
+const SOURCE_DIRECTORY = 'js';
+const TEMPLATE_DIRECTORY = 'templates';
+
 
 var webpackConfig = 
 {
@@ -145,9 +148,11 @@ gulp.task('develop:info', ['develop:mkdir'], function(callback)
  */
 gulp.task('develop:link', ['develop:mkdir'], function()
 {
-    return gulp.src([CONFIGURATION_FILE, RESOURCES_DIRECTORY])
+    return gulp.src([CONFIGURATION_FILE, RESOURCES_DIRECTORY, SOURCE_DIRECTORY, PLUGINS_DIRECTORY])
                .pipe(symlink([DEVELOP_WEBROOT + '/' + CONFIGURATION_FILE,
-                              DEVELOP_WEBROOT + '/' + RESOURCES_DIRECTORY], {force: true}));
+                              DEVELOP_WEBROOT + '/' + RESOURCES_DIRECTORY,
+                              NODE_MODULES_DIRECTORY + '/' + SOURCE_DIRECTORY,
+                              NODE_MODULES_DIRECTORY + '/' + PLUGINS_DIRECTORY], {force: true}));
 });
 
 /**
@@ -159,8 +164,14 @@ gulp.task('develop', ['develop:mkdir', 'develop:config', 'develop:link', 'develo
     var server = new WebpackDevServer(compiler, webpackServerConfig);
     server.listen(DEVELOP_PORT, DEVELOP_HOST, function(err)
     {
+        console.log('');
+        console.log('==========');
         console.log('Starting server on: http://' + DEVELOP_HOST + ':' + DEVELOP_PORT);
         console.log('Serving: ' + DEVELOP_WEBROOT);
+        console.log('');
+        console.log('Make sure ' + DEVELOP_HOST + ':' + DEVELOP_PORT + ' is allowed access to the Rodan server');
+        console.log('==========');
+        console.log('');
     });
 });
 
