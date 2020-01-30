d28ce03987c080245b3319fc0a4353132e94fdf6
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-01-30 09:43:52 -0500

    Fix infinite recursion problem

--- src/js/Views/Master/Navigation/ViewNavigationNodeProject.js
+++ src/js/Views/Master/Navigation/ViewNavigationNodeProject.js
@@ -2,6 +2,7 @@ import Backbone from 'backbone';
 import RODAN_EVENTS from 'js/Shared/RODAN_EVENTS';
 import NAV_EVENTS from './Events';
 import Radio from 'backbone.radio';
+import Project from 'js/Models/Project';
 import ViewNavigationNodeRunJobs from './ViewNavigationNodeRunJobs';
 import ViewNavigationNodeResourceLists from './ViewNavigationNodeResourceLists';
 import ViewNavigationNodeResources from './ViewNavigationNodeResources';
@@ -46,7 +47,7 @@ export default class ViewNavigationNodeProject extends ViewNavigationNode
      * @param {BaseModel} model instance of 'Resource', 'Workflow', 'WorkflowRun', or 'RunJob'
      * @return {ViewNavigationNode} a subclass of ViewNavigationNode; only returns class, not instance; will return ViewNavigationNode if the BaseModel provided is not one of the above
      */
-    getChildView(model)
+    childView(model)
     {
         switch (model.get('name'))
         {
