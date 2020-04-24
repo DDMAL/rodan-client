a55cf7f09f1510ba7a7cd8421ec603f6d2e342ee
Juliette Regimbal <juliette.regimbal@gmail.com>
2020-04-24 13:57:10 -0400

    Alert on changing label name if name is used

--- src/js/Views/Master/Main/ResourceLabel/ViewResourceLabel.js
+++ src/js/Views/Master/Main/ResourceLabel/ViewResourceLabel.js
@@ -25,15 +25,21 @@ export default class ViewResourceLabel extends Marionette.View
     _handleClickButtonSave()
     {
         let newName = $(this.ui.nameInput).val();
-        this.model.save({name: newName}, {patch: true, success: () => {
-          Radio.channel('rodan').request(RODAN_EVENTS.REQUEST__RESOURCES_UPDATE_LABELS);
-          Radio.channel('rodan').request(RODAN_EVENTS.REQUEST__MODAL_HIDE);
-        }});
+        this.model.save({name: newName}, {
+            patch: true,
+            success: () => {
+                Radio.channel('rodan').request(RODAN_EVENTS.REQUEST__RESOURCES_UPDATE_LABELS);
+                Radio.channel('rodan').request(RODAN_EVENTS.REQUEST__MODAL_HIDE);
+            },
+            error: () => {
+                $(this.ui.alert).removeAttr('hidden');
+            }});
     }
 }
 ViewResourceLabel.prototype.ui = {
     buttonSave: '#button-save_label_name',
-    nameInput: '#text-label_name'
+    nameInput: '#text-label_name',
+    alert: '#alert-label_exists'
 };
 ViewResourceLabel.prototype.events = {
     'click @ui.buttonSave': '_handleClickButtonSave'
